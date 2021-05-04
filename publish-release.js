5ff5ef3ff46590c8cb2685ef7468d47f878df185
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-04 20:44:39 +0200

    ci: add publish-release job

---
+++ publish-release.js
@@ -0,0 +1,8 @@
+const { execSync } = require('child_process');
+
+const changedPackages = JSON.parse(execSync('lerna changed --json', { encoding: 'utf-8' }));
+execSync(`git commit -m "chore(release): publish release" -a`);
+changedPackages.forEach(changed => {
+  execSync(`git tag ${ changed.name }@${ changed.version }`);
+});
+execSync(`lerna publish from-package --yes`);
