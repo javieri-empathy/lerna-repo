6e2f4007e9ea0764c344a9c6b867a21597304feb
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-05 22:14:34 +0200

    feat: restore suggestion

--- packages/x-components/src/index.ts
+++ packages/x-components/src/index.ts
@@ -1,7 +1,11 @@
-import { Result } from '@empathytest/search-types';
+import { Result, Suggestion } from '@empathytest/search-types';
 
 export const result: Result = {
   id: '1',
   title: 'A result',
   images: []
+}
+
+export const suggestion: Suggestion = {
+  query: 'a query'
 }
