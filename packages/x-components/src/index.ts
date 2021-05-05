4a4a09cd4b9b8aea051ea2e034dce4980edec3d3
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-05 22:19:27 +0200

    feat: remove suggestion impl
    BREAKING CHANGE: remove suggestion impl

--- packages/x-components/src/index.ts
+++ packages/x-components/src/index.ts
@@ -1,11 +1,7 @@
-import { Result, Suggestion } from '@empathytest/search-types';
+import { Result } from '@empathytest/search-types';
 
 export const result: Result = {
   id: '1',
   title: 'A result',
   images: []
-}
-
-export const suggestion: Suggestion = {
-  query: 'a query'
 }
