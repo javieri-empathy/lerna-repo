89b474e7b8c91dd198b44925074441570a68d829
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-05 16:44:40 +0200

    ci: update prepare release script

--- prepare-release.js
+++ prepare-release.js
@@ -1,9 +1,9 @@
 const { exec } = require('./utils');
 
 const [kind = 'stable'] = process.argv.slice(2);
-const preReleaseArgument = kind === 'alpha' ? '--conventional-prerelease ' : '';
+const releaseKindArgument = kind === 'alpha' ? '--conventional-prerelease' : '--conventional-graduate';
 
 [
-  `lerna version --conventional-commits --no-git-tag-version --yes ${ preReleaseArgument }`,
+  `lerna version --conventional-commits --no-git-tag-version --yes ${ releaseKindArgument }`,
   'git commit -m "chore(release): prepare release" -a'
 ].forEach(exec);
