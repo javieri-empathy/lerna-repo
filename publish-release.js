3850c7268f3721aef0355c5480d46d0ea5db2ffa
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-05 15:45:37 +0200

    feat: update publish release

--- publish-release.js
+++ publish-release.js
@@ -1,9 +1,7 @@
-const { execSync } = require('child_process');
+const { exec } = require('./utils');
 
-const changedPackages = JSON.parse(execSync('lerna changed --json', { encoding: 'utf-8' }));
-execSync(`lerna publish from-package --yes`, { encoding: 'utf-8' });
-execSync(`git add packages/`, { encoding: 'utf-8' });
-execSync(`git commit -m "chore(release): publish release"`, { encoding: 'utf-8' });
+const changedPackages = JSON.parse(exec('lerna changed --json'));
+exec(`lerna publish from-package --yes`);
 changedPackages.forEach(changed => {
-  execSync(`git tag ${ changed.name }@${ changed.version }`);
+  exec(`git tag ${ changed.name }@${ changed.version }`);
 });
