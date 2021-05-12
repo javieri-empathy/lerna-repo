553991fa885bf1ad0a4f29056680a175f20e2807
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-12 21:15:25 +0200

    ci: add new prepare release process

--- packages/logger/src/logger.ts
+++
@@ -1,5 +0,0 @@
-import { Logger, LogLevel } from './logger.interfaces';
-import { SimpleLogger } from './simple-logger';
-
-export const logger: Logger = new SimpleLogger();
-logger.serverLevel = LogLevel.silent;
--- prepare-release.js
+++ prepare-release.js
@@ -1,9 +1,14 @@
 const { exec } = require('./utils');
 
+const branchName = 'release';
 const [releaseKind = 'stable'] = process.argv.slice(2);
 const releaseKindArgument = releaseKind === 'alpha' ? '--conventional-prerelease' : '--conventional-graduate';
-
-[
-  `lerna version --conventional-commits --no-git-tag-version --yes ${ releaseKindArgument }`,
-  `git commit -m "chore(release): prepare ${releaseKind} release" -a`
-].forEach(exec);
+const branchesOutput = exec('git branch -r').trim();
+const remoteBranches = branchesOutput.split('\n').map(branch => branch.trim());
+console.log(remoteBranches);
+if (remoteBranches.includes(`origin/${branchName}`)) {
+  exec(`git push --delete origin ${ branchName }`);
+}
+// exec(`git checkout -b ${ branchName }`);
+exec(`lerna version --conventional-commits --no-git-tag-version --yes ${ releaseKindArgument }`);
+// exec(`git commit -m "chore(release): prepare ${releaseKind} release" -a`);
