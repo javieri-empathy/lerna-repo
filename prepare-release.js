48480768f71ab3fd4b2c4ff2ae8d627f5482c8c2
Javier Iglesias Garcia <javieri@empathy.co>
2021-05-12 22:18:09 +0200

    ci: update ci

--- prepare-release.js
+++ prepare-release.js
@@ -1,13 +1,20 @@
 const { exec } = require('./utils');
 
-const branchName = 'release';
-const [releaseKind = 'stable'] = process.argv.slice(2);
-const releaseKindArgument = releaseKind === 'alpha' ? '--conventional-prerelease' : '--conventional-graduate';
-const branchesOutput = exec('git branch -r').trim();
-const remoteBranches = branchesOutput.split('\n').map(branch => branch.trim());
-if (remoteBranches.includes(`origin/${branchName}`)) {
-  exec(`git push --delete origin ${ branchName }`);
+
+// removeReleaseBranchIfExists('release');
+execLernaVersion();
+
+
+function removeReleaseBranchIfExists(branchName) {
+  const branchesOutput = exec('git branch -r').trim();
+  const remoteBranches = branchesOutput.split('\n').map(branch => branch.trim());
+  if (remoteBranches.includes(`origin/${branchName}`)) {
+    exec(`git push --delete origin ${ branchName }`);
+  }
 }
-// exec(`git checkout -b ${ branchName }`);
-exec(`lerna version --conventional-commits --no-git-tag-version --yes ${ releaseKindArgument }`);
-//exec(`git commit -m "chore(release): prepare ${releaseKind} release" -a`);
+
+function execLernaVersion() {
+  const [releaseKind = 'stable'] = process.argv.slice(2);
+  const releaseKindArgument = releaseKind === 'alpha' ? '--conventional-prerelease' : '--conventional-graduate';
+  exec(`lerna version --conventional-commits --no-git-tag-version --yes ${ releaseKindArgument }`);
+}
