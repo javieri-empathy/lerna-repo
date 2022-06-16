8c7d4938ca7a2ab05592d02ae0418ce2132b2953
Javier Iglesias <javieri@empathy.co>
2022-06-16 09:20:24 +0100

    Use rest methods

--- .github/actions/download-dist-files/index.js
+++ .github/actions/download-dist-files/index.js
@@ -3,7 +3,7 @@ const github = require("@actions/github");
 const { execSync } = require("child_process");
 
 (async function downloadDistFiles() {
-  const artifacts = await github.actions.listWorkflowRunArtifacts({
+  const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
     owner: context.repo.owner,
     repo: context.repo.repo,
     run_id: github.event.workflow_run.id,
@@ -11,7 +11,7 @@ const { execSync } = require("child_process");
   const matchArtifact = artifacts.data.artifacts.find(
     (artifact) => artifact.name === "dist-files"
   );
-  const download = await github.actions.downloadArtifact({
+  const download = await github.rest.actions.downloadArtifact({
     owner: context.repo.owner,
     repo: context.repo.repo,
     artifact_id: matchArtifact.id,
