e12c4277337f2f12afa3702559880d51bffb04e7
Javier Iglesias <javieri@empathy.co>
2022-06-16 11:51:45 +0100

    Use octokit client

--- .github/actions/download-dist-files/index.js
+++ .github/actions/download-dist-files/index.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
+const core = require("@actions/core");
 const github = require("@actions/github");
 const { execSync } = require("child_process");
 
 (async function downloadDistFiles() {
-  const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
+  const githubToken = core.getInput("token");
+  const octokit = github.getOctokit(githubToken);
+  const artifacts = await octokit.rest.actions.listWorkflowRunArtifacts({
     owner: context.repo.owner,
     repo: context.repo.repo,
     run_id: github.event.workflow_run.id,
@@ -11,7 +14,7 @@ const { execSync } = require("child_process");
   const matchArtifact = artifacts.data.artifacts.find(
     (artifact) => artifact.name === "dist-files"
   );
-  const download = await github.rest.actions.downloadArtifact({
+  const download = await octokit.rest.actions.downloadArtifact({
     owner: context.repo.owner,
     repo: context.repo.repo,
     artifact_id: matchArtifact.id,
